1ece27609f2ddde16b49c13dc962e010cdde77dc
dhfpswlqkd <rlatkddb4356@naver.com>
2025-01-30 18:40:54 +0000

    :sparkles: Feat: Add Video component (refs #9)

---
+++ vue/video-filter/src/components/Video.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <video v-if="videoUrl" ref="videoPlayer" controls>
+            <source :src="videoUrl" type="video/mp4" />
+            브라우저가 비디오 태그를 지원하지 않습니다.
+        </video>
+    </div>
+</template>
+  
+<script>
+    export default {
+        name: "VideoComponent",
+        props: {
+            videoUrl: String,
+        },
+        methods: {
+            seekToTime(time) {
+                if (this.$refs.videoPlayer) {
+                    this.$refs.videoPlayer.currentTime = time;
+                    this.$refs.videoPlayer.play();
+                }
+            }
+        },
+    };
+</script>
+  
+<style>
+</style>
+  
